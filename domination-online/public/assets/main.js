37ca88c35e00a66fe47f9cf45e795427059b9875
OmerDislitas <dislitas20@gmail.com>
2025-08-25 12:45:18 +0300

    last update

--- domination-online/public/assets/main.js
+++ domination-online/public/assets/main.js
@@ -344,7 +344,124 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   // Örnek #land verisi
-  
+  window.landData = `
+7 -4 1
+7 -3 0
+8 -3 0
+7 -2 0
+6 -2 0
+8 -2 0
+7 -1 0
+8 -1 0
+7 0 0
+6 1 0 tower
+6 0 0
+6 -1 0
+5 -2 0
+6 -3 0
+6 -4 1
+5 1 0
+5 2 0
+5 3 0
+5 4 0
+7 1 0
+8 2 0
+7 3 0
+8 4 0
+5 -1 0
+4 0 0 strong_tower
+3 1 0
+3 2 0
+2 3 0
+3 4 0
+3 6 0
+9 0 0 strong_tower
+9 1 0
+10 2 0
+10 3 0
+10 4 0
+10 6 0
+5 -3 0
+7 -5 0
+6 -5 1
+8 -4 0
+5 5 0
+5 6 0
+7 5 0
+8 6 0
+5 7 0
+5 8 0
+7 7 0
+8 8 0
+2 7 0
+3 5 0
+9 5 0
+10 7 0
+10 8 0
+3 8 0
+2 9 0
+3 10 0
+3 11 0
+3 12 0
+2 13 0
+3 14 0
+3 15 0
+4 16 0 strong_tower
+4 17 0
+5 9 0
+5 10 0
+5 11 0
+5 12 0
+5 13 0
+5 14 0
+5 15 0
+6 16 0
+7 9 0
+8 10 0
+7 11 0
+8 12 0
+7 13 0
+8 14 0
+6 15 0 tower
+7 15 0
+7 16 0
+5 17 0
+6 17 0
+7 17 0
+10 9 0
+10 10 0
+9 11 0
+10 12 0
+10 13 0
+10 14 0
+9 15 0
+9 16 0 strong_tower
+5 18 0
+6 18 1
+7 18 0
+8 18 0
+4 19 0
+5 19 0
+6 19 1
+7 19 0
+8 19 0
+5 20 0
+6 20 2
+7 20 2
+8 20 0
+5 21 0
+6 21 2
+7 21 0
+6 8 0
+7 8 0
+8 17 0
+4 -1 0
+4 -3 0
+5 -4 0
+5 -5 0
+
+`.trim().split('\n');
+
   // Move unitData outside or make it global
   window.unitData = `
 
@@ -365,7 +482,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // Unit power levels
   window.unitPower = { peasant: 1, spearman: 2, swordsman: 3, knight: 4 };
 
-  const BUILDING_PROT = { tower: 2, strong_tower: 3 };
+  const BUILDING_PROT = { tower: 2, strong_tower: 3, castle: 1 };
 
 
   function centerCameraOnMap() {
